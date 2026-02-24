7f978dc5fafd327d72641b2740647f5789576e0b
asmabkr <167143334+asmabkr@users.noreply.github.com>
2026-02-25 00:36:03 +0100

    small change

--- stt_whisper/whisper_listener.py
+++ stt_whisper/whisper_listener.py
@@ -2,7 +2,7 @@ import whisper
 import torch
 
 class MediBotListener:
-    def __init__(self, model_size="tiny"):
+    def __init__(self, model_size="base"):
         # Utilise le GPU si disponible pour réduire les 5s de latence
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = whisper.load_model(model_size, device=device)
